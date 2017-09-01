c94d10d6e8e27d8c80b9e5d1acef3ef9d2c7efa8
alejandrofduran <alejandrofduran@gmail.com>
2017-09-01 17:00:17 -0300

    1 iteración de eliminación de código
    Se eliminó el código repetido de la funciones de test 1 y 2

--- 01-CodigoRepetido/Java/src/idiom/IdiomTest.java
+++ 01-CodigoRepetido/Java/src/idiom/IdiomTest.java
@@ -13,53 +13,77 @@ package idiom;
 import junit.framework.TestCase;
 
 public class IdiomTest extends TestCase {
-	
-	protected CustomerBook customerBook;
 
-	public void setUp(){
-		customerBook = new CustomerBook ();
-	}
-	
-	public void testAddingCustomerShouldNotTakeMoreThan50Milliseconds(){
-		
-		long millisecondsBeforeRunning = System.currentTimeMillis();
-		customerBook.addCustomerNamed("John Lennon");
-		long millisecondsAfterRunning = System.currentTimeMillis();
-		
-		assertTrue( (millisecondsAfterRunning-millisecondsBeforeRunning) < 50 );
-	}
+    protected CustomerBook customerBook;
+    long millisecondsBeforeRunning, millisecondsAfterRunning;
 
-	public void testRemovingCustomerShouldNotTakeMoreThan100Milliseconds(){
-		String paulMcCartney = "Paul McCartney";
-		
-		customerBook.addCustomerNamed(paulMcCartney);
+    public void setUp() {
+        customerBook = new CustomerBook();
+    }
 
-		long millisecondsBeforeRunning = System.currentTimeMillis();
-		customerBook.removeCustomerNamed(paulMcCartney);
-		long millisecondsAfterRunning = System.currentTimeMillis();
-		
-		assertTrue( (millisecondsAfterRunning-millisecondsBeforeRunning) < 100 );
-	}
-	
-	public void testCanNotAddACustomerWithEmptyName (){
-		
-		try {
-			customerBook.addCustomerNamed("");
-			fail();
-		} catch (RuntimeException e) {
-			assertEquals(e.getMessage(),CustomerBook.CUSTOMER_NAME_EMPTY);
-			assertTrue(customerBook.isEmpty());
-		}
-	}
+    public void testAddingCustomerShouldNotTakeMoreThan50Milliseconds() {
 
-	public void testCanNotRemoveNotAddedCustomers (){
-		
-		try {
-			customerBook.removeCustomerNamed("John Lennon");
-			fail();
-		} catch (IllegalArgumentException e) {
-			assertEquals(e.getMessage(),CustomerBook.INVALID_CUSTOMER_NAME);
-			assertEquals(0,customerBook.numberOfCustomers());
-		}
-	}
+        countMillisecondsStart();
+        customerBook.addCustomerNamed("John Lennon");
+        countMillisecondsEnd();
+
+        assertTrue(elapsedTimeLessThan(50));
+    }
+
+    public void testRemovingCustomerShouldNotTakeMoreThan100Milliseconds() {
+        String paulMcCartney = "Paul McCartney";
+
+        customerBook.addCustomerNamed(paulMcCartney);
+
+        countMillisecondsStart();
+        customerBook.removeCustomerNamed(paulMcCartney);
+        countMillisecondsEnd();
+
+        assertTrue(elapsedTimeLessThan(100));
+    }
+
+    public void testCanNotAddACustomerWithEmptyName() {
+
+        try {
+            customerBook.addCustomerNamed("");
+            fail();
+        } catch (RuntimeException e) {
+            assertEquals(e.getMessage(), CustomerBook.CUSTOMER_NAME_EMPTY);
+            assertTrue(customerBook.isEmpty());
+        }
+    }
+
+    public void testCanNotRemoveNotAddedCustomers() {
+
+        try {
+            customerBook.removeCustomerNamed("John Lennon");
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals(e.getMessage(), CustomerBook.INVALID_CUSTOMER_NAME);
+            assertEquals(0, customerBook.numberOfCustomers());
+        }
+    }
+
+    private void resetTimers() {
+        millisecondsBeforeRunning = 0;
+        millisecondsAfterRunning = 0;
+    }
+
+    private void countMillisecondsStart() {
+        resetTimers();
+        millisecondsBeforeRunning = System.currentTimeMillis();
+    }
+
+    private void countMillisecondsEnd() {
+        millisecondsAfterRunning = System.currentTimeMillis();;
+    }
+
+    private long elapsedTime() {
+        return (millisecondsAfterRunning - millisecondsBeforeRunning);
+    }
+
+    private boolean elapsedTimeLessThan(long milliseconds) {
+        return elapsedTime() < milliseconds;
+    }
+
 }
